49b896b22750a453dd650cbf475076400fba4592
MarkBrooks1985 <alynnnari@gmail.com>
2022-10-19 17:47:38 +1030

    8th commit, work imported

---
+++ src/components/Work.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Work() {
+  // return (
+  //   <section>
+  //       <tr>
+  //               <td style="width:70px;padding-right:10px;" class="about-title"><span id="about-work"><h1>Work</h1></span></td>
+  //               <td style="width:6px;background-color:var(--border-color);">&nbsp;</td>
+  //               <td style="padding-left:10px;" class="about-me">
+  //                     <a href="https://rouge86.github.io/Nibbles/"><img class="img-container" src="Assets/Images/nibbles.png" alt="Nibbles"></a><br>
+  //                     <a href="https://markbrooks1985.github.io/Horiseon-Example-challenge-Demo/"><img class="img-container" src="Assets/Images/debug.png" alt="Work2"></a>
+  //                     <a href="https://mysterious-journey-75550.herokuapp.com/"><img class="img-container" src="Assets/Images/codeIt.png" alt="Work3"></a><br>
+  //                     <a href="#"><img className="img-container" src="Assets/Images/comingsoon.png" alt="Work4"></a>
+  //                     <a href="#"><img className="img-container" src="Assets/Images/comingsoon.png" alt="Work5"></a></td>
+  //           </tr>
+  //           </section>
+  // )
+}
