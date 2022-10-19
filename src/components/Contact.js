20859fa7dd607abb961b014cdd7f8cfa95161413
MarkBrooks1985 <alynnnari@gmail.com>
2022-10-19 17:47:52 +1030

    9th commit, contact imported

---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Contact() {
+  // return (
+  //   <section>
+  //       <tr>
+  //           <td style="width:70px;padding-right:10px;" class="about-title"><span id="about-contact"><h1>Contact Me</h1></span></td>
+  //           <td style="width:6px;background-color:var(--border-color);">&nbsp;</td>
+  //           <td style="padding-left:10px;" className="about-me">
+  //               <div className="container">
+  //                   <form action="action_page.php">
+  //                     <label for="fname">Full Name</label><br/>
+  //                     <input type="text" id="fname" name="fullname" placeholder="Your Full name.." /><br/>
+  //                     <label for="email">Email</label><br/>
+  //                     <input type="text" id="email" name="email" placeholder="Your email.."><br>
+  //                     <label for="subject">Subject</label><br>
+  //                     <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea><br>
+  //                     <input type="submit" value="Submit">
+  //                   </form>
+  //                 </div>
+  //           </td>
+  //       </tr>
+  //       </section>
+  // )
+}
